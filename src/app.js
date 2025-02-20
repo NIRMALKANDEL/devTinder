bf43cda553c60c4b737801c36260d161e737f156
Nirmal <nirmalkandel001@gmail.com>
2025-02-20 11:53:11 +0530

    EXPLORING HTTP CALLS

--- src/app.js
+++ src/app.js
@@ -6,12 +6,24 @@ app.use("/test", (req, res) => {
   res.send("hello form the server !! ");
 });
 
-app.get("/user ", (req, res) => {
-  res.sent({
+app.get("/user", (req, res) => {
+  res.send({
     firstname: "nirmal ",
     lastname: "kandel ",
     status: "user info was sent ",
   });
+});
+app.post("/user", (req, res) => {
+  console.log("save data to the datsabase");
+  res.send("data succesfully saved in data base");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("deleted suceesfully ");
+});
+app.patch("/user", (req, res) => {
+  console.log("patched data is updated   to the datsabase");
+  res.send("data succesfully updated in data base");
 });
 
 app.listen(7777, () => {
